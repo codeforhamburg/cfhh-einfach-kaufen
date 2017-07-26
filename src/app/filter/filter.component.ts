c46d47ea0c6e3ebbd5858581d76e1f08f42e4bd3
umut0 <tas@we-build.city>
2017-07-26 03:59:47 +0200

    animations

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -11,12 +11,12 @@ import {trigger, state, animate, style, transition} from '@angular/animations';
   animations: [trigger(
       'slideInOutAnimation', [
         transition(':enter', [
-          style({transform: 'translateX(-100%)', opacity: 0.5}),
-          animate('250ms', style({transform: 'translateX(0)', opacity: 1}))
+          style({transform: 'translateX(-100%)', opacity: 0.5, position: 'absolute', width: '100%'}),
+          animate('250ms', style({transform: 'translateX(0)', opacity: 1, width: '100%'}))
         ]),
         transition(':leave', [
-          style({transform: 'translateX(0)', opacity: 1, position: 'absolute', top: '20px'}),
-          animate('250ms', style({transform: 'translateX(300%)', opacity: 0.5, position: 'absolute', top: '20px'}))
+          style({transform: 'translateX(0)', opacity: 1, position: 'absolute', width: '100%'}),
+          animate('250ms', style({transform: 'translateX(300%)', opacity: 0.5, position: 'absolute', width: '100%'}))
         ])
       ]
     )]
