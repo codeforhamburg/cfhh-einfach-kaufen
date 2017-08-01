e17b9023b1589367de4db044d1eee1a214a2df9d
apptum <apptum@gmail.com>
2017-08-01 12:00:16 +0200

    wrong name in import

--- src/app/services/mapBoxGeocoder.service.spec.ts
+++ src/app/services/mapBoxGeocoder.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, inject } from '@angular/core/testing';
 
-import { MapBoxGeocoderService } from './geocoder.service';
+import { MapBoxGeocoderService } from './mapBoxGeocoder.service';
 
 describe('MapBoxGeocoderService', () => {
   beforeEach(() => {
