8ecb03bfca3a075d8268d73a0e31db3168eb844d
apptum <apptum@gmail.com>
2017-08-09 13:58:08 +0200

    impressum, footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,18 +6,17 @@ import {trigger, state, animate, style, transition} from '@angular/animations';
   selector: 'wbc-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
-  animations: [trigger(
-      'slideInAnimation', [
-        transition(':enter', [
-          style({transform: 'translateY(100%)', opacity: 0}),
-          animate('300ms', style({transform: 'translateY(0)', opacity: 1}))
-        ]),
-        transition(':leave', [
-          style({transform: 'translateY(0)', opacity: 1}),
-          animate('300ms', style({transform: 'translateY(100%)', opacity: 0}))
-        ])
-      ]
-    )
+  animations: [
+    trigger('slideInAnimation', [
+      transition(':enter', [
+        style({transform: 'translateY(100%)', opacity: 0}),
+        animate('300ms', style({transform: 'translateY(0)', opacity: 1}))
+      ]),
+      transition(':leave', [
+        style({transform: 'translateY(0)', opacity: 1}),
+        animate('300ms', style({transform: 'translateY(100%)', opacity: 0}))
+      ])
+    ])
   ]
 })
 export class FooterComponent implements OnInit {
